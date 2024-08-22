4df0bf3f404a7d68dadaa16a4daad63fba02bc0a
Valeria <valeria.mamaiko@gmail.com>
2024-08-22 13:22:48 +0300

    pageparser initial commit

---
+++ src/main/java/org/techtask/Main.java
@@ -0,0 +1,11 @@
+package org.techtask;
+
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+
+    }
+}
---
+++ src/main/java/org/techtask/parse/PageParser.java
@@ -0,0 +1,5 @@
+package org.techtask.parse;
+
+
+public class PageParser {
+}
